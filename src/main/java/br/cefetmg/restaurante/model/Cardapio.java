c22c8620d33f6943300b19887c4b3420f984509f
ivinercassio <ivinercassio@gmail.com>
2026-03-29 22:49:05 -0300

    create repositories

--- src/main/java/br/cefetmg/restaurante/model/Cardapio.java
+++ src/main/java/br/cefetmg/restaurante/model/Cardapio.java
@@ -2,12 +2,30 @@ package br.cefetmg.restaurante.model;
 
 import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "cardapio")
 public class Cardapio {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = true)
     private EnumDia descricao;
+    
+    @Column(nullable = false)
     private List<Receita> receitas;
 }
---
+++ src/main/java/br/cefetmg/restaurante/repository/ReceitaRepository.java
@@ -0,0 +1,11 @@
+package br.cefetmg.restaurante.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.cefetmg.restaurante.model.Receita;
+
+@Repository
+public interface ReceitaRepository extends JpaRepository<Long, Receita> {
+    
+} 
