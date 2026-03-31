b59ee79592805fc20e8e2ed9361779d9bb1cd4fe
ivinercassio <ivinercassio@gmail.com>
2026-03-30 23:29:53 -0300

    create cardapio endpoints

---
+++ src/main/java/br/cefetmg/restaurante/controller/CardapioController.java
@@ -0,0 +1,55 @@
+package br.cefetmg.restaurante.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.cefetmg.restaurante.model.Cardapio;
+import br.cefetmg.restaurante.service.CardapioService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/cardapios")
+public class CardapioController {
+
+    private final CardapioService cardapioService;
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Cardapio> get(@PathVariable Long id) {
+        Cardapio cardapio = cardapioService.get(id);
+        return ResponseEntity.ok().body(cardapio);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Cardapio>> getAll() {
+        List<Cardapio> lista = cardapioService.getAll();
+        return ResponseEntity.ok().body(lista);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Cardapio> insert(@RequestBody Cardapio cardapio) {
+        Cardapio registro = cardapioService.insert(cardapio);
+        return ResponseEntity.ok().body(registro);
+    }
+
+    @PutMapping("")
+    public ResponseEntity<Cardapio> update(@RequestBody Cardapio cardapio) {
+        Cardapio registro = cardapioService.update(cardapio);
+        return ResponseEntity.ok().body(registro);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Cardapio> delete(@PathVariable Long id) {
+        Cardapio cardapio = cardapioService.delete(id);
+        return ResponseEntity.ok().body(cardapio);
+    }
+}
---
+++ src/main/java/br/cefetmg/restaurante/service/CardapioService.java
@@ -0,0 +1,47 @@
+package br.cefetmg.restaurante.service;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.cefetmg.restaurante.model.Cardapio;
+import br.cefetmg.restaurante.repository.CardapioRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CardapioService {
+    
+    private final CardapioRepository cardapioRepository;
+
+    public Cardapio get(Long id) {
+        Cardapio cardapio = cardapioRepository.findById(id).orElse(null);
+        if (cardapio == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi encontrado Cardapio com o id: " + id);
+        return cardapio;
+    }
+
+    public List<Cardapio> getAll() {
+        return cardapioRepository.findAll();
+    }
+
+    public Cardapio insert(Cardapio cardapio) {
+        cardapio.setId(null);
+        return cardapioRepository.save(cardapio);
+    }
+
+    public Cardapio update(Cardapio cardapio) {
+        if (cardapio.getId() == null)
+            throw new ResponseStatusException(HttpStatus.BAD_GATEWAY, "O campo id é obrigatório.");
+        get(cardapio.getId()); // verifica se existe registro com este id
+        return cardapioRepository.save(cardapio);
+    }
+
+    public Cardapio delete(Long id) {
+        Cardapio cardapio = get(id); // verifica se existe registro com este id
+        cardapioRepository.deleteById(id);
+        return cardapio;
+    }
+}
