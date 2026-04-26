8aa961c3da431d2bc9a091bf087cd1af6cf7ab8c
ivinercassio <ivinercassio@gmail.com>
2026-04-26 13:04:47 -0300

    preAuthorize CardapioController

--- src/main/java/br/cefetmg/restaurante/controller/CardapioController.java
+++ src/main/java/br/cefetmg/restaurante/controller/CardapioController.java
@@ -3,6 +3,7 @@ package br.cefetmg.restaurante.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,30 +25,35 @@ public class CardapioController {
     private final CardapioService cardapioService;
     
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('CARDAPIO_GET')")
     public ResponseEntity<Cardapio> get(@PathVariable Long id) {
         Cardapio cardapio = cardapioService.get(id);
         return ResponseEntity.ok().body(cardapio);
     }
 
     @GetMapping("")
+    @PreAuthorize("hasAuthority('CARDAPIO_GET')")
     public ResponseEntity<List<Cardapio>> getAll() {
         List<Cardapio> lista = cardapioService.getAll();
         return ResponseEntity.ok().body(lista);
     }
 
     @PostMapping("")
+    @PreAuthorize("hasAuthority('CARDAPIO_POST')")
     public ResponseEntity<Cardapio> insert(@RequestBody Cardapio cardapio) {
         Cardapio registro = cardapioService.insert(cardapio);
         return ResponseEntity.ok().body(registro);
     }
 
     @PutMapping("")
+    @PreAuthorize("hasAuthority('CARDAPIO_PUT')")
     public ResponseEntity<Cardapio> update(@RequestBody Cardapio cardapio) {
         Cardapio registro = cardapioService.update(cardapio);
         return ResponseEntity.ok().body(registro);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('CARDAPIO_DELETE')")
     public ResponseEntity<Cardapio> delete(@PathVariable Long id) {
         Cardapio cardapio = cardapioService.delete(id);
         return ResponseEntity.ok().body(cardapio);
