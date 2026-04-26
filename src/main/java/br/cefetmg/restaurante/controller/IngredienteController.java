f8c7d294d68e4ef50f9862142ab8a041af993e83
ivinercassio <ivinercassio@gmail.com>
2026-04-26 13:06:29 -0300

    preAuthorize IngredienteController

--- src/main/java/br/cefetmg/restaurante/controller/IngredienteController.java
+++ src/main/java/br/cefetmg/restaurante/controller/IngredienteController.java
@@ -3,6 +3,7 @@ package br.cefetmg.restaurante.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,30 +25,35 @@ public class IngredienteController {
     private final IngredienteService ingredienteService;
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('INGREDIENTE_GET')")
     public ResponseEntity<Ingrediente> get(@PathVariable Long id) {
         Ingrediente ingrediente = ingredienteService.get(id);
         return ResponseEntity.ok().body(ingrediente);
     }
 
     @GetMapping("")
+    @PreAuthorize("hasAuthority('INGREDIENTE_GET')")
     public ResponseEntity<List<Ingrediente>> getAll() {
         List<Ingrediente> lista = ingredienteService.getAll();
         return ResponseEntity.ok().body(lista);
     }
 
     @PostMapping("")
+    @PreAuthorize("hasAuthority('INGREDIENTE_POST')")
     public ResponseEntity<Ingrediente> insert(@RequestBody Ingrediente ingrediente) {
         Ingrediente registro = ingredienteService.insert(ingrediente);
         return ResponseEntity.ok().body(registro);
     }
 
     @PutMapping("")
+    @PreAuthorize("hasAuthority('INGREDIENTE_PUT')")
     public ResponseEntity<Ingrediente> update(@RequestBody Ingrediente ingrediente) {
         Ingrediente registro = ingredienteService.update(ingrediente);
         return ResponseEntity.ok().body(registro);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('INGREDIENTE_DELETE')")
     public ResponseEntity<Ingrediente> delete(@PathVariable Long id) {
         Ingrediente ingrediente = ingredienteService.delete(id);
         return ResponseEntity.ok().body(ingrediente);
