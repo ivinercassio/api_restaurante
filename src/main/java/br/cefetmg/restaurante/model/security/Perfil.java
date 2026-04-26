209773c1ae9a10263b7f8fd2d09d441d5d04091f
ivinercassio <ivinercassio@gmail.com>
2026-04-26 13:37:32 -0300

    create data.sql to insertions

--- src/main/java/br/cefetmg/restaurante/model/security/Perfil.java
+++ src/main/java/br/cefetmg/restaurante/model/security/Perfil.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
-@Entity
+@Entity(name = "perfil")
 public class Perfil {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
