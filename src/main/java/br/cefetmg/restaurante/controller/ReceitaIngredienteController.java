f19f127c1e0827b84d524f537fbdb7ead24488ef
ivinercassio <ivinercassio@gmail.com>
2026-04-26 13:11:19 -0300

    preAuthorize ReceitaIngredienteController

--- src/main/java/br/cefetmg/restaurante/controller/ReceitaIngredienteController.java
+++ src/main/java/br/cefetmg/restaurante/controller/ReceitaIngredienteController.java
@@ -3,6 +3,7 @@ package br.cefetmg.restaurante.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,36 +25,42 @@ public class ReceitaIngredienteController {
 
     // insert == update, jah que o update de quantidade eh sobrescrevendo o valor
     @PostMapping("/insert")
+    @PreAuthorize("hasAuthority('RECEITAINGREDIENTE_POST')")
     public ResponseEntity<ReceitaIngredienteDTO> addIngrediente(@RequestBody ReceitaIngredienteDTO dto) {
         ReceitaIngredienteDTO relacao = relacaoService.addIngrediente(dto);
         return ResponseEntity.ok().body(relacao);
     }
     
     @DeleteMapping("/delete/receita/{idReceita}/ingrediente/{idIngrediente}")
+    @PreAuthorize("hasAuthority('RECEITAINGREDIENTE_DELETE')")
     public ResponseEntity<ReceitaIngredienteDTO> addIngrediente(@PathVariable Long idReceita, @PathVariable Long idIngrediente) {
         ReceitaIngredienteDTO relacao = relacaoService.removeIngrediente(idReceita, idIngrediente);
         return ResponseEntity.ok().body(relacao);
     }
 
     @GetMapping("")
+    @PreAuthorize("hasAuthority('RECEITAINGREDIENTE_GET')")
     public ResponseEntity<List<ReceitaIngredienteDTO>> getAll() {
         List<ReceitaIngredienteDTO> lista = relacaoService.getAll();
         return ResponseEntity.ok().body(lista);
     }
     
     @GetMapping("/receita/{id}")
+    @PreAuthorize("hasAuthority('RECEITAINGREDIENTE_GET')")
     public ResponseEntity<List<ReceitaIngredienteDTO>> getAllByReceitaId(@PathVariable Long id) {
         List<ReceitaIngredienteDTO> lista = relacaoService.getAllByReceitaId(id);
         return ResponseEntity.ok().body(lista);
     }
 
     @GetMapping("/ingrediente/{id}")
+    @PreAuthorize("hasAuthority('RECEITAINGREDIENTE_GET')")
     public ResponseEntity<List<ReceitaIngredienteDTO>> getAllByIngredienteId(@PathVariable Long id) {
         List<ReceitaIngredienteDTO> lista = relacaoService.getAllByIngredienteId(id);
         return ResponseEntity.ok().body(lista);
     }
 
     @GetMapping("/receita/{idReceita}/ingrediente/{idIngrediente}")
+    @PreAuthorize("hasAuthority('RECEITAINGREDIENTE_GET')")
     public ResponseEntity<ReceitaIngredienteDTO> getById(@PathVariable Long idReceita, @PathVariable Long idIngrediente) {
         ReceitaIngredienteDTO relacao = relacaoService.getById(idReceita, idIngrediente);
         return ResponseEntity.ok().body(relacao);
